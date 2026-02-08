64a40f4e0f3921651246b216afa74e2a1c2baae5
CloverForby <rustypennyten@gmail.com>
2026-02-08 18:41:15 +0800

    init

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const mysql = require('mysql/promise')
+require('dotenv').config();
+const port = 3000;
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0
+}
+
+const app = express();
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log('Server running on port', port);
+})
+
+app.get('/allcards', async(req, res)=>{
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err){
+        console.error(err)
+        res.status(500).json({message: 'Server error for allcards'});
+    }
+})
+
+app.post('/allcards', async(req, res)=>{
+    const {card_name, card_pic} = req.body;
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO cards(card_name,  card_pic) VALEUS (?, ?)', [card_name, card_pic]);
+        res.status(201).json({message: 'Card '+card_name+ ' added successfully'})
+    } catch (err){
+        console.error(err)
+        res.status(500).json({message: 'Server error - could not add card '+ card_name});
+    }
+})
+
+app.delete('/deletecard/:id', async(req, res)=>{
+    const {id} = req.params;
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('DELETE FROM defaultdb.cards WHERE id=' + id);
+        res.status(201).json({message: 'Card '+card_name+ ' deleted successfully'})
+    } catch (err){
+        console.error(err)
+        res.status(500).json({message: 'Server error - could not delete card '+ card_name});
+    }
+})
+
+app.put('/updatecard/:id', async(req, res)=>{
+    const {id} = req.params;
+    const {card_name, card_pic} = req.body
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('UPDATE cards SET card_name=?, card_pic=? WHERE id=?', [card_name, card_pic,id]);
+        res.status(201).json({message: 'Card '+card_name+ ' updated successfully'})
+    } catch (err){
+        console.error(err)
+        res.status(500).json({message: 'Server error - could not update card '+ card_name});
+    }
+})
